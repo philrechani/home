824f96b64e4e3f42ba5c89d786ec9a4d3d1b9099
corruptvision <3matoro45@gmail.com>
2024-01-16 18:34:05 -0500

    fixed the svg element location for homepage

--- src/Earth.js
+++ src/Earth.js
@@ -4,11 +4,11 @@ import { geoPath, geoEqualEarth, geoGraticule } from 'd3'
 const projection = geoEqualEarth()
 const path = geoPath(projection)
 
-export const Earth = ({ data: { countries, interiors }}) => {
+export const Earth = ({ data: { countries, interiors } }) => {
 
     return (
         <div className='worldMap-container'>
-            <svg className='worldMap-svg'>
+            <svg className='worldMap-svg' version="1.1" width="100%" height="100%" viewBox="0 0 1024 768" preserveAspectRatio="xMidYMid meet">
                 <g className='worldMap' /* transform="scale(2)" */>
                     <path className="sphere" d={path({ type: 'Sphere' })} />
 
@@ -30,3 +30,4 @@ export const Earth = ({ data: { countries, interiors }}) => {
         </div>
     )
 }
+
